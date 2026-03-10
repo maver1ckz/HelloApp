c32f5853196087ef3fdb0c0a57bf9141fe73913e
maver1ckz <as9998@srmist.edu.in>
2026-03-10 09:24:19 +0530

    Coded UC1: Display Hello World in console

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,11 @@
+/**
+ * HelloApp.java - A simple Java application that Displays "Hello World!" to the console.
+ *
+ * @author Developer Name
+ * @version 1.0
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
